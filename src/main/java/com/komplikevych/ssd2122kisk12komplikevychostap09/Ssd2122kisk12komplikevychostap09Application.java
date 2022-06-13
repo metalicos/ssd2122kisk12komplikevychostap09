0f6169b7a4a1638a92f61d7cba442a1a33855a08
Ostap Komplikevych <ostap.ja@gmail.com>
2022-06-13 22:56:54 +0300

    [ssd2122kisk12komplikevychostap09] add spring boot

---
+++ src/main/java/com/komplikevych/ssd2122kisk12komplikevychostap09/Ssd2122kisk12komplikevychostap09Application.java
@@ -0,0 +1,13 @@
+package com.komplikevych.ssd2122kisk12komplikevychostap09;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ssd2122kisk12komplikevychostap09Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ssd2122kisk12komplikevychostap09Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/komplikevych/ssd2122kisk12komplikevychostap09/Ssd2122kisk12komplikevychostap09ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.komplikevych.ssd2122kisk12komplikevychostap09;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ssd2122kisk12komplikevychostap09ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
